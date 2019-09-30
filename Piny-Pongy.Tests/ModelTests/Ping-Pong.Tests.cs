db84e9aaf74a3ee777fcd4e44ff122c432a49c44
Christine Frank and sofia giordano <sofia@>
2019-09-30 10:14:20 -0700

    Successfully add test to check num range

--- Piny-Pongy.Tests/ModelTests/Ping-Pong.Tests.cs
+++ Piny-Pongy.Tests/ModelTests/Ping-Pong.Tests.cs
@@ -1,4 +1,6 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Linq;
 using PingyPongy;
 
 namespace PingyPongy.Tests
@@ -25,6 +27,16 @@ namespace PingyPongy.Tests
         {
             PingPong testPingPong = new PingPong();
             Assert.AreEqual(-1, testPingPong.ConvertToNum("-7"));
+        }
+
+        [TestMethod]
+        public void OutputPingPong_DisplayNumRange_NumberRange()
+        {
+            PingPong testPingPong = new PingPong();
+            List<int> expected = new List<int>() { 0, 1, 2, 3, 4 };
+            // Cannot use .AreEqual(new List<int> ... , ..OutputPingPong(4)) because compares 2 objects that are different - need to compare internal values
+            // Use .SequenceEquals (!! *Import System.Linq* !!) to iterate through list and check values & order
+            Assert.IsTrue(expected.SequenceEqual(testPingPong.OutputPingPong(4)));
         }
     }
 }
