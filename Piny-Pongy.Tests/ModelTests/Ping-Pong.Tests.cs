5550bbc6cd5b401c667f3840c20831f0c2971b9c
Christine Frank and sofia giordano <christine@>
2019-09-30 09:53:36 -0700

    run second test successfully

--- Piny-Pongy.Tests/ModelTests/Ping-Pong.Tests.cs
+++ Piny-Pongy.Tests/ModelTests/Ping-Pong.Tests.cs
@@ -12,5 +12,19 @@ namespace PingyPongy.Tests
             PingPong testPingPong = new PingPong();
             Assert.AreEqual(4, testPingPong.ConvertToNum("4"));
         }
+
+        [TestMethod]
+        public void ConvertToNum_CheckForNumber_InvalidString()
+        {
+            PingPong testPingPong = new PingPong();
+            Assert.AreEqual(-1, testPingPong.ConvertToNum("Invalid input"));
+        }
+
+        // [TestMethod]
+        // public void ConvertToNum_CheckForNumber_InvalidString()
+        // {
+        //     PingPong testPingPong = new PingPong();
+        //     Assert.AreEqual("Invalid input", testPingPong.ConvertToNum("Invalid input"));
+        // }
     }
 }
